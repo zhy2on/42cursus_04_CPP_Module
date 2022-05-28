d5f07e8862e8a9451ffbb2b39c95752a0bb2c720
zhy2on <dhwlgus5940@gmail.com>
2022-05-28 17:34:44 +0900

    Fix: eof 입력 처리

--- cpp00/ex01/main.cpp
+++ cpp00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jihoh <jihoh@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/24 20:10:03 by jihoh             #+#    #+#             */
-/*   Updated: 2022/05/28 17:18:22 by jihoh            ###   ########.fr       */
+/*   Updated: 2022/05/28 17:32:50 by jihoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,18 @@ int main()
 
 	std::cout << "\033[0;32mjihoh's phonebook\033[0m" << std::endl;
 	std::cout << "\033[0;33mEnter a command: [ADD SEARCH EXIT]\033[0m" << std::endl << "> ";
-	std::cin >> cmd;
-	while (cmd.compare("EXIT"))
+	while (1)
 	{
-		if (!cmd.compare("ADD"))
+		std::cin >> cmd;
+		if (std::cin.eof() || !cmd.compare("EXIT"))
+			break ;
+		else if (!cmd.compare("ADD"))
 			phonebook.add_contact();
 		else if (!cmd.compare("SEARCH"))
 			phonebook.search_contact();
 		else
 			std::cout << "\033[0;31mInvalid command\033[0m" << std::endl;
 		std::cout << "\033[0;33mEnter a command: [ADD SEARCH EXIT]\033[0m" << std::endl << "> ";
-		std::cin >> cmd;
 	}
 	return (0);
 }
