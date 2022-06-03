b68d432481dde71661648c0d0989675483b20d25
zhy2on <dhwlgus5940@gmail.com>
2022-06-03 16:19:48 +0900

    Feat: cpp02-02 함수, 연산자 오버로딩, const 위치에 따른 의미 연습

---
+++ cpp02/ex02/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jihoh <jihoh@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/01 20:54:49 by jihoh             #+#    #+#             */
+/*   Updated: 2022/06/03 15:42:36 by jihoh            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int main( void )
+{
+	Fixed a;
+	Fixed const b(Fixed(5.05f) * Fixed(2));
+
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+
+	std::cout << b << std::endl;
+
+	std::cout << Fixed::max(a, b) << std::endl;
+	
+	return 0;
+}
