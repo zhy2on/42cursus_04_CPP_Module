96281004e3a0aec28706987f6d72adc5d3e8c41e
zhy2on <dhwlgus5940@gmail.com>
2022-06-09 14:56:42 +0900

    Chore: borderlands 캐릭터 설정 맞춰 main 변경

--- cpp03/ex00/main.cpp
+++ cpp03/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jihoh <jihoh@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/06 17:34:43 by jihoh             #+#    #+#             */
-/*   Updated: 2022/06/06 20:38:17 by jihoh            ###   ########.fr       */
+/*   Updated: 2022/06/09 14:55:13 by jihoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 int	main()
 {
-	ClapTrap ai("AI");
+	ClapTrap cl4pTP("cl4pTP");
 
-	ai.attack("jihoh");
-	ai.takeDamage(6);
-	ai.beRepaired(4);
-	ai.takeDamage(3);
-	ai.beRepaired(8);
-	ai.takeDamage(17);
-	ai.attack("jihoh");
-	ai.beRepaired(1);
+	cl4pTP.attack("Calypso Twins");
+	cl4pTP.takeDamage(6);
+	cl4pTP.beRepaired(4);
+	cl4pTP.takeDamage(3);
+	cl4pTP.beRepaired(8);
+	cl4pTP.takeDamage(17);
+	cl4pTP.attack("Calypso Twins");
+	cl4pTP.beRepaired(1);
 }
