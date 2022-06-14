d714a86f9649e4cb7f88066c076398e5ec8de57e
zhy2on <dhwlgus5940@gmail.com>
2022-06-14 15:56:09 +0900

    Feat: cpp04-03 예제 추가

--- cpp04/ex03/srcs/main.cpp
+++ cpp04/ex03/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jihoh <jihoh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 15:09:25 by jihoh             #+#    #+#             */
-/*   Updated: 2022/06/14 13:04:32 by jihoh            ###   ########.fr       */
+/*   Updated: 2022/06/14 15:55:44 by jihoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,36 @@ int main()
 
 		me->use(0, *bob);
 		me->use(1, *bob);
+
+		delete bob;
+		delete me;
+		delete src;
+
+		system("leaks materia");
+	}
+	{
+		IMateriaSource *src = new MateriaSource();
+		src->learnMateria(new Ice());
+		src->learnMateria(new Cure());
+
+		ICharacter *me = new Character("me");
+		AMateria *tmp;
+
+		for (int i = 0; i < 4; i++)
+		{
+			tmp = src->createMateria("ice");
+			me->equip(tmp);
+		}
+		tmp = src->createMateria("cure");
+		me->equip(tmp);
+
+		ICharacter *bob = new Character("bob");
+		me->use(3, *bob);
+
+		tmp = src->createMateria("cure");
+		me->equip(tmp);
+		me->use(3, *bob);
+
 
 		delete bob;
 		delete me;
