49e07cdce3a5ed1041f06fd6fcbd5f2ec4d76471
zhy2on <dhwlgus5940@gmail.com>
2022-06-21 17:12:19 +0900

    cpp06-02 dynamic cast 연습

---
+++ cpp06/ex02/srcs/main.cpp
@@ -0,0 +1,100 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jihoh <jihoh@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/21 15:53:17 by jihoh             #+#    #+#             */
+/*   Updated: 2022/06/21 17:11:59 by jihoh            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cstdlib>
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+Base *generate(void)
+{
+	int p = rand() % 3;
+	if (p == 0)
+		return new A();
+	else if (p == 1)
+		return new B();
+	else if (p == 2)
+		return new C();
+	else
+		throw new std::exception();
+}
+
+void identify(Base *p)
+{
+	A *a = dynamic_cast<A *>(p);
+	if (a)
+	{
+		std::cout << "A" << std::endl;
+		return ;
+	}
+	B *b = dynamic_cast<B *>(p);
+	if (b)
+	{
+		std::cout << "B" << std::endl;
+		return ;
+	}
+	C *c = dynamic_cast<C *>(p);
+	if (c)
+	{
+		std::cout << "C" << std::endl;
+		return ;
+	}
+	
+}
+
+void identify(Base &p)
+{
+	try
+	{
+		A &a = dynamic_cast<A &>(p);
+		std::cout << "A" << std::endl;
+		(void)a;
+	}
+	catch(const std::exception& e) {}
+	try
+	{
+		B &b = dynamic_cast<B &>(p);
+		std::cout << "B" << std::endl;
+		(void)b;
+	}
+	catch(const std::exception& e) {}
+	try
+	{
+		C &c = dynamic_cast<C &>(p);
+		std::cout << "C" << std::endl;
+		(void)c;
+	}
+	catch(const std::exception& e) {}
+}
+
+int main(void)
+{
+	Base	*base;
+
+	std::cout << "Identifying with pointers" << std::endl;
+	for (int i = 0; i < 9; i++)
+	{
+		base = generate();
+		identify(base);
+		delete base;
+	}
+
+	std::cout << std::endl << "Identifying with references" << std::endl;
+	for (int i = 0; i < 9; i++)
+	{
+		base = generate();
+		identify(*base);
+		delete base;
+	}
+	// system("leaks identify");
+}
