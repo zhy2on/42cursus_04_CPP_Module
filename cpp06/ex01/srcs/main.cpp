50ed0718f05013bcbc0f7176e5d88152a252de58
zhy2on <dhwlgus5940@gmail.com>
2022-06-21 15:15:40 +0900

    cpp06-01 시리얼라이즈

---
+++ cpp06/ex01/srcs/main.cpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jihoh <jihoh@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/20 23:43:51 by jihoh             #+#    #+#             */
+/*   Updated: 2022/06/21 15:15:17 by jihoh            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "data.hpp"
+#include <cstdint>
+#include <iostream>
+
+uintptr_t serialize(Data *ptr)
+{
+	return reinterpret_cast<uintptr_t>(ptr);
+}
+
+Data *deserialize(uintptr_t raw)
+{
+	return reinterpret_cast<Data *>(raw);
+}
+
+int main(void)
+{
+	Data *ptr = new Data;
+	Data *ptr2;
+	uintptr_t raw;
+
+	ptr->data = 42;
+	raw = serialize(ptr);
+	ptr2 = deserialize(raw);
+	std::cout << "ptr->data: " << ptr->data << std::endl;
+	std::cout << "ptr2->data: " << ptr2->data << std::endl;
+	ptr->data = 24;
+	std::cout << "ptr->data: " << ptr->data << std::endl;
+	std::cout << "ptr2->data: " << ptr2->data << std::endl;
+
+	return 0;
+}
