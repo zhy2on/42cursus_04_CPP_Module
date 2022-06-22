3c061f266cbdc6b31008a4cc2a8a5086fa3dce2d
zhy2on <dhwlgus5940@gmail.com>
2022-06-22 19:49:29 +0900

    Fix: copy constructor 에서 assignment operator 사용하도록

--- cpp02/ex00/srcs/Fixed.cpp
+++ cpp02/ex00/srcs/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: jihoh <jihoh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 19:45:59 by jihoh             #+#    #+#             */
-/*   Updated: 2022/06/22 19:45:05 by jihoh            ###   ########.fr       */
+/*   Updated: 2022/06/22 19:48:46 by jihoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ Fixed::Fixed()
 Fixed::Fixed(const Fixed &fixed)
 {
 	std::cout << "Copy constructor called" << std::endl;
-	this->fixed_point_value = fixed.getRawBits();
+	*this = fixed;
 }
 
 Fixed::~Fixed()
