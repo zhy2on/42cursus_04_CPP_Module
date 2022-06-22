9890672e0f619d4e8e31b3463d6320d42a65b0c5
zhy2on <dhwlgus5940@gmail.com>
2022-06-22 16:55:10 +0900

    Chore

--- cpp01/ex01/srcs/main.cpp
+++ cpp01/ex01/srcs/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jihoh <jihoh@student.42seoul.kr>           +#+  +:+       +#+        */
+/*   By: jihoh <jihoh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 20:59:39 by jihoh             #+#    #+#             */
-/*   Updated: 2022/05/30 21:19:39 by jihoh            ###   ########.fr       */
+/*   Updated: 2022/06/22 16:54:36 by jihoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	main()
 
 	for (int i = 0; i < 6; i++)
 	{
-		zombies = zombie.zombieHorde(i, name[i]);
+		zombies = zombie.zombieHorde(i + 1, name[i]);
 		delete[] zombies;
 	}
 	// system("leaks zombieHord");
