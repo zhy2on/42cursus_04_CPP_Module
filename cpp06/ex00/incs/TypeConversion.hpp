ed078d591f94736b890d76f46a4b33b64519dc6a
zhy2on <dhwlgus5940@gmail.com>
2022-07-01 16:45:03 +0900

    Chore: 헤더 수정

--- cpp06/ex00/incs/TypeConversion.hpp
+++ cpp06/ex00/incs/TypeConversion.hpp
@@ -6,7 +6,7 @@
 /*   By: jihoh <jihoh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/19 18:08:31 by jihoh             #+#    #+#             */
-/*   Updated: 2022/07/01 16:09:21 by jihoh            ###   ########.fr       */
+/*   Updated: 2022/07/01 16:44:54 by jihoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,6 @@ private:
 	};
 
 	void checkValidInput(const char *literal);
-	bool setSpecialLiteralType(const char *literal);
 	void setLiteralType(const char *literal);
 
 	TypeConversion(void);
